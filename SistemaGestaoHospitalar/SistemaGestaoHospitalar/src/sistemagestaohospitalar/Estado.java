142f3055977ae80cbfc4e6f0e27a502266c49274
MaluAbrami <marialuizaabrami@gmail.com>
2024-12-05 20:04:06 -0300

    add lógica ao enum de estado de atendimento

--- SistemaGestaoHospitalar/SistemaGestaoHospitalar/src/sistemagestaohospitalar/Estado.java
+++ SistemaGestaoHospitalar/SistemaGestaoHospitalar/src/sistemagestaohospitalar/Estado.java
@@ -1,10 +1,20 @@
 package sistemagestaohospitalar;
 
 public enum Estado {
-    Entrada,
-    Tratamento_Clinico_Geral,
-    Preparacao_Pre_Cirurgia,
-    Cirurgia,
-    Pos_Cirurgia,
-    Alta_Clinica;
+    Entrada("Recebeu entrada no Hospital."),
+    Tratamento_Clinico_Geral("Encaminhado para consulta com Clinico Geral."),
+    Preparacao_Pre_Cirurgia("Encaminhado para a pre-cirurgia."),
+    Cirurgia("Encaminhado para a Cirurgia."),
+    Pos_Cirurgia("Encaminhado para a pos-cirurgia."),
+    Alta_Clinica("Encaminhado para a Alta Clinica.");
+    
+    private final String descricao;
+    
+    Estado(String descricao){
+        this.descricao = descricao;
+    }
+    
+    public String getDescricao(){
+        return descricao;
+    }
 }

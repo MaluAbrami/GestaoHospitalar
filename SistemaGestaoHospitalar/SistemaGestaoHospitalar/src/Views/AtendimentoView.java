6a39633c9cd974235745d1564475bccf8997eb7a
MaluAbrami <marialuizaabrami@gmail.com>
2024-12-06 12:56:15 -0300

    update atendimentoView

--- SistemaGestaoHospitalar/SistemaGestaoHospitalar/src/Views/AtendimentoView.java
+++ SistemaGestaoHospitalar/SistemaGestaoHospitalar/src/Views/AtendimentoView.java
@@ -13,6 +13,46 @@ public class AtendimentoView {
     }
     
     public void menu(){
+        int op;
         
+        do{
+            System.out.println("--==[Atendimentos]==--");
+            System.out.println("1 - Cadastrar");
+            System.out.println("2 - Alterar");
+            System.out.println("3 - Excluir");
+            System.out.println("4 - Consultar");
+            System.out.println("5 - Relatorio");
+            System.out.println("6 - Voltar ao menu principal");
+            System.out.println("Opcao: ");
+            
+            op = scanner.nextInt();
+            scanner.skip("\n");
+            
+            switch(op){
+                case 1:{
+                    atendimentoService.cadastrar();
+                    break;
+                }
+                case 2:{
+                    break;
+                }
+                case 3:{
+                    break;
+                }
+                case 4:{
+                    break;
+                }
+                case 5:{
+                    break;
+                }
+                case 6:{
+                    System.out.println("Voltando para o menu principal...");
+                    break;
+                }
+                default:{
+                    System.out.println("Opcao invalida, tente novamente");
+                }
+            }
+        } while(op != 6);
     }
 }

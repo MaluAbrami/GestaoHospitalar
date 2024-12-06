890e2977600a07231d092fc694da92649e181601
MaluAbrami <marialuizaabrami@gmail.com>
2024-12-06 12:55:07 -0300

    create novo cadastrar para caso diferente

--- SistemaGestaoHospitalar/SistemaGestaoHospitalar/src/Service/AtendimentoService.java
+++ SistemaGestaoHospitalar/SistemaGestaoHospitalar/src/Service/AtendimentoService.java
@@ -4,6 +4,7 @@ import DaoImpl.AtendimentoDaoImpl;
 import DaoImpl.MedicoDaoImpl;
 import DaoImpl.PacienteDaoImpl;
 import java.util.List;
+import java.util.Scanner;
 import sistemagestaohospitalar.Atendimento;
 import sistemagestaohospitalar.Medico;
 import sistemagestaohospitalar.Paciente;
@@ -12,25 +13,25 @@ public class AtendimentoService {
     private AtendimentoDaoImpl atendimentoDao;
     private MedicoDaoImpl medicoDao;
     private PacienteDaoImpl pacienteDao;
+    private Scanner scanner;
     
-    public AtendimentoService(AtendimentoDaoImpl atendimentoDao, MedicoDaoImpl medicoDao, PacienteDaoImpl pacienteDao){
+    public AtendimentoService(AtendimentoDaoImpl atendimentoDao, MedicoDaoImpl medicoDao, PacienteDaoImpl pacienteDao, Scanner scanner){
         this.atendimentoDao = atendimentoDao;
         this.medicoDao = medicoDao;
         this.pacienteDao = pacienteDao;
+        this.scanner = scanner;
     }
     
-    public void cadastrar(String crm, Paciente p){
+    public void cadastrar(String crm, Paciente p){ //sera usado quando o atendimento for realizado junto ao cadastro do paciente
         List<Medico> medicosExistentes = medicoDao.listar();
         Paciente procuraPaciente = pacienteDao.buscar(p.getCpf());
         
         if(medicosExistentes.isEmpty()){
             System.out.print("A lista de medicos cadastrados no sistema esta vazia");
-        } else if(procuraPaciente == null){
-            System.out.print("O paciente informado nao existe");
         } 
         
         for(Medico m: medicosExistentes){
-            if(m.getCrm().equals(m.getCrm())){
+            if(m.getCrm().equals(crm)){
                 Atendimento at = new Atendimento();
                 atendimentoDao.adicionar(at);
                 at.adicionarMedico(m); //adiciona o medico ao atendimento
@@ -40,5 +41,37 @@ public class AtendimentoService {
             }
         }
         System.out.println("Erro: nao tem nenhum medico cadastrado com essa crm no sistema");
+    }
+    
+    public void cadastrar(){
+        List<Medico> medicosExistentes = medicoDao.listar();
+        String crmMedico, cpfPaciente;
+        
+        if(medicosExistentes.isEmpty()){
+            System.out.print("A lista de medicos cadastrados no sistema esta vazia");
+        } else{
+            System.out.println("Informe o cpf do paciente: ");
+            cpfPaciente = scanner.nextLine();
+            Paciente pacienteExistente = pacienteDao.buscar(cpfPaciente);
+            
+            if(pacienteExistente != null){
+                System.out.println("Informe o C.R.M. do medico que sera designado: ");
+                crmMedico = scanner.nextLine();
+
+                for (Medico m : medicosExistentes) {
+                    if (m.getCrm().equals(m.getCrm())) {
+                        Atendimento at = new Atendimento();
+                        atendimentoDao.adicionar(at);
+                        at.adicionarMedico(m); //adiciona o medico ao atendimento
+                        at.adicionarPaciente(pacienteExistente); //adiciona o paciente ao atendimento
+                        System.out.println(at.getEstadoAtual().getDescricao());
+                        System.out.println("Paciente: " + pacienteExistente.getNome() + ", Medico designado: " + m.getNome() + ", Especialidade do medico: " + m.getEspecialidade());
+                    }
+                }
+                System.out.println("Erro: nao tem nenhum medico cadastrado com essa crm no sistema");
+            } else{
+                System.out.println("Erro: nao tem nenhum paciente cadastrado com o cpf informado");
+            }
+        }
     }
 }
